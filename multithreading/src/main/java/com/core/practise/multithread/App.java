cd52a28f15b84a8f12e083effce099a726e8b16b
digvendra <digvendra@hotmail.com>
2019-03-24 14:51:40 +0530

    created maven project multithreading for practise

---
+++ multithreading/src/main/java/com/core/practise/multithread/App.java
@@ -0,0 +1,13 @@
+package com.core.practise.multithread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
