5559fdd3fa9a794525d191165926a42376ce480d
digvendra <digvendra@hotmail.com>
2019-03-26 23:22:03 +0530

    created java8newfeature

---
+++ java8newfeature/src/main/java/com/core/practise/java8newfeature/App.java
@@ -0,0 +1,13 @@
+package com.core.practise.java8newfeature;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
